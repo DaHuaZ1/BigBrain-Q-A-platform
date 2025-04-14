83c534a8ca7eb3fe35c1c4f6e4813f280b8dc8f7
Yuxin Li <z5586569@cse.unsw.edu.au>
2025-04-14 18:51:19 +1000

    Initialize Login

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 const Login = () => {
     return (
         <div>Login Page</div>
