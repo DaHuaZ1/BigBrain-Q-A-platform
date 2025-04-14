c0d8ede3a2de6b165d1f38c2d3d1e1e94144075e
Yuxin Li <z5586569@cse.unsw.edu.au>
2025-04-15 00:24:44 +1000

    Add an error message for login

--- frontend/src/components/loginPage.jsx
+++ frontend/src/components/loginPage.jsx
@@ -6,9 +6,11 @@ import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
 import AUTH from "../Constant";
 import { useNavigate } from "react-router-dom";
+import Alert from '@mui/material/Alert';
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("")
   const navigate = useNavigate()
   const signin = async () => {
     const url = "http://localhost:5005/admin/auth/login"
@@ -28,14 +30,19 @@ const Login = (props) => {
       console.log("token saved")
       props.setToken(data.token)
       navigate("/")
+    }else{
+      setError(data.error || "Legendary Secret Key Login Failed")
     }
   }
   return (
-    <Container maxWidth="xs">
+    <Container>
       <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
         <Typography variant="h5" gutterBottom>
                     Welcome! Please login to continue
         </Typography>
+        {error &&(
+          <Alert severity="error">{error}</Alert>
+        )}
         <TextField
           required
           id="outlined-required"
