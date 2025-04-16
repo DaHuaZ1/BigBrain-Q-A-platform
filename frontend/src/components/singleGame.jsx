ab4102765c889953e995607deb89cbfcbcd4397e
Mingxuan,Zhang <mingxuan.zhang1@my.jcu.edu.au>
2025-04-16 21:58:18 +1000

    add update game function

--- frontend/src/components/singleGame.jsx
+++ frontend/src/components/singleGame.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
+import { useParams } from "react-router-dom";
+import { fetchAllGames } from "../getAllGames";
+import { putNewGame } from "../putNewGame";
+import { useNavigate } from "react-router-dom";
 
 const SingleGame = () => {
+    const { game_id } = useParams();
+    const [name, setName] = useState("");
+    const [thumbnail, setThumbnail] = useState("");
+    const [games, setGames] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetchAllGames()
+            .then((data) => {
+                const foundGame = data.games.find((game) => game.id === parseInt(game_id));
+                if (foundGame) {
+                    setName(foundGame.name);
+                    setThumbnail(foundGame.thumbnail);
+                }
+            })
+            .catch((error) => console.error("Error fetching games:", error));
+    },[]);
+
+    const updateGame = () => {
+        fetchAllGames()
+            .then((data) => {
+                const oldGames = data.games || [];
+                const updatedGames = oldGames.map(game => 
+                    game.id === parseInt(game_id) ? { ...game, name, thumbnail } : game
+                );
+                return putNewGame(updatedGames);
+            })
+            .then(() => {
+                navigate("/dashboard");
+                setName("");
+                setThumbnail("");
+            })
+            .catch((error) => console.error("Error updating game:", error));
+    }
+
     return (
         <Container maxWidth="sm" sx={{
             height: '100vh',
@@ -34,14 +73,18 @@ const SingleGame = () => {
               </Typography>
 
               <TextField
-                label="Game Title"
+                label="Game Name"
                 id="outlined-required"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
               />
               <TextField
                 label="Thumbnail(URL)"
                 id="outlined-required"
+                onChange={(e) => setThumbnail(e.target.value)}
+                value={thumbnail}
               />
-              <Button variant="contained">
+              <Button variant="contained" onClick={updateGame}>
                 Save Changes
               </Button>
             </Box>
