7a2b193839b704874cdf480904108edd77e25f6b
Yuxin Li <z5586569@cse.unsw.edu.au>
2025-04-17 23:15:31 +1000

    Update HomePage

--- frontend/src/components/homePage.jsx
+++ frontend/src/components/homePage.jsx
@@ -1,9 +1,37 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
 const Home = (props) => {
+  const navigate = useNavigate();
+  const handleJoinClick = () => {
+    navigate('/play');
+  };
+
+  const handleDashboardClick = () => {
+    navigate('/dashboard');
+  };
+
   return (
     <>
       {props.token ===null
-        ?<>What are you waiting for? Hurry up and join us!</>
-        :<>Welcome back, ready to start a new game?</>}
+        ?<>
+          <p>
+          What are you waiting for? Hurry up and join us!
+          </p>
+          <Button variant="contained" color="primary" onClick={handleJoinClick}>
+            Join a Game
+          </Button>
+        </>
+
+        :<>
+          <p>
+          Welcome back, ready to start a new game?
+          </p>
+          <Button variant="contained" color="primary" onClick={handleDashboardClick}>
+            Go to Dashboard
+          </Button>
+        </>}
     </>
   )
 }
