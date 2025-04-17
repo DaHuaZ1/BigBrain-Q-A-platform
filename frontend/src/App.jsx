d3f16131af2224f513d961e37399a1dd5377be14
Mingxuan,Zhang <mingxuan.zhang1@my.jcu.edu.au>
2025-04-17 17:23:17 +1000

    wrap router by snackbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,22 +6,25 @@ import { useState } from 'react';
 import Home from './components/homePage';
 import Dashboard from './components/Dashboard';
 import SingleGame from './components/singleGame';
+import { SnackbarProvider } from 'notistack';
 
 function App() {
   const[token, setToken]=useState(localStorage.getItem('token'));
   return (
     // Route Jump Related Code
-    <BrowserRouter>
-      <Bar token={token} setToken={setToken} />
-      <Routes>
-        <Route path="/" element={<Home token={token}/>} />
-        <Route path="/login" element={<Login setToken={setToken}/>} />
-        <Route path="/signup" element={<Signup setToken={setToken}/>} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/game/:game_id" element={<SingleGame />} />
-      </Routes>
+    <SnackbarProvider maxSnack={3} autoHideDuration={1000} anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
+      <BrowserRouter>
+        <Bar token={token} setToken={setToken} />
+        <Routes>
+          <Route path="/" element={<Home token={token}/>} />
+          <Route path="/login" element={<Login setToken={setToken}/>} />
+          <Route path="/signup" element={<Signup setToken={setToken}/>} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/game/:game_id" element={<SingleGame />} />
+        </Routes>
 
-    </BrowserRouter>
+      </BrowserRouter>
+    </SnackbarProvider>
   )
 }
 
