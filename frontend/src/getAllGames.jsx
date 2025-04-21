0e732d35e6223a35f6ccc0de69c2f312f7ffa91b
Mingxuan,Zhang <mingxuan.zhang1@my.jcu.edu.au>
2025-04-21 16:47:24 +1000

    meet lint requirement

--- frontend/src/getAllGames.jsx
+++ frontend/src/getAllGames.jsx
@@ -1,23 +1,23 @@
 import AUTH from "./Constant";
 
 const handleError = (res) => {
-    const {error} = res;
-    if (error) {
-        throw new Error(error);
-    }
-    return res;
+  const {error} = res;
+  if (error) {
+    throw new Error(error);
+  }
+  return res;
 }
 
 export const fetchAllGames = () => {
-    const userToken = localStorage.getItem(AUTH.Token_key);
+  const userToken = localStorage.getItem(AUTH.Token_key);
 
-    return fetch("http://localhost:5005/admin/games", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8",
-            Authorization: `Bearer ${userToken}`,
-        },
-    })
-        .then((res) => res.json())
-        .then(handleError)
+  return fetch("http://localhost:5005/admin/games", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${userToken}`,
+    },
+  })
+    .then((res) => res.json())
+    .then(handleError)
 };
