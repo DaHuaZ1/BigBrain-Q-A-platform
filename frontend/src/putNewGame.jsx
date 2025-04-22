c0e5a04b9ca801de27ab6225ff420be0719e0a99
Mingxuan,Zhang <mingxuan.zhang1@my.jcu.edu.au>
2025-04-22 21:12:48 +1000

    add some comments

--- frontend/src/putNewGame.jsx
+++ frontend/src/putNewGame.jsx
@@ -1,24 +1,30 @@
+// Import constant keys for localStorage token access
 import AUTH from "./Constant";
 
+// Handle errors returned from the backend response
 const handleError = (res) => {
-  const {error} = res;
+  const { error } = res;
   if (error) {
+    // Throw an error to be caught by the caller if the response contains an error
     throw new Error(error);
   }
-  return res;
-}
+  return res; // Return the valid response
+};
 
+// Function to overwrite all games on the server with a new games array
 export const putNewGame = (games) => {
+  // Retrieve the user token from localStorage
   const userToken = localStorage.getItem(AUTH.Token_key);
 
+  // Send PUT request to update the full list of games
   return fetch("http://localhost:5005/admin/games", {
-    method: "PUT",
+    method: "PUT", // HTTP method for replacing existing data
     headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-      Authorization: `Bearer ${userToken}`,
+      "Content-Type": "application/json; charset=UTF-8", // Specify JSON payload
+      Authorization: `Bearer ${userToken}`, // Include token in Authorization header for authentication
     },
-    body: JSON.stringify({games: games}),
+    body: JSON.stringify({ games: games }), // Convert games array to JSON string
   })
-    .then((res) => res.json())
-    .then(handleError)
+    .then((res) => res.json()) // Parse response as JSON
+    .then(handleError); // Handle potential errors in the response
 };
